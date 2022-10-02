ec4dd25c61dc8421237aca6e1735dad9614da1ab
Sebastian Czech <sebaczech@gmail.com>
2022-10-02 16:06:01 +0200

    Fix integration tests

--- infra/tests/module_network_integration_test/main.tf
+++ infra/tests/module_network_integration_test/main.tf
@@ -2,11 +2,14 @@ module "network" {
   source = "./../../modules/network"
 
   compartment_id            = var.compartment_id
-  vcn_name                  = var.vcn_name
-  vcn_cidr_block            = var.vcn_cidr_block
-  subnet_private_cidr_block = var.subnet_private_cidr_block
-  subnet_public_cidr_block  = var.subnet_public_cidr_block
-  route_rules               = var.route_rules
-  egress_security_rules     = var.egress_security_rules
-  ingress_security_rules    = var.ingress_security_rules
+  networks = {
+    "test" : {
+      vcn_name : var.vcn_name,
+      vcn_cidr_block : var.vcn_cidr_block,
+      subnet_private_cidr_block : var.subnet_private_cidr_block,
+      subnet_public_cidr_block : var.subnet_public_cidr_block,
+      egress_security_rules : var.egress_security_rules,
+      ingress_security_rules : var.ingress_security_rules,
+    }
+  }
 }
